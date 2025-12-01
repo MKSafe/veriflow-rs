f597beac60f56934132ca8d8dd203ab68732b11f
NewLifeStyle101 <damian.myszko@gmail.com>
2025-12-01 01:09:28 +0000

    feat: implemented the minimal server which prints a connected user's address and the number of currently connected users'

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,17 +1,37 @@
-mod Server{
-    use tokio::net::TcpListener
-    pub struct Listener{
-        host: String
-        port: String
-        listener: Option<TcpListener>
-        header_buffer:[u8; 4]
-        buffer: [u8; 500]
-        open_connections: Vec<T>
-    }
 
-    impl Listener{
-        fn new(host:&str,port:&str,){
-            
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tracing::info;
+use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::io;
+pub struct Listener{
+    listener: TcpListener,
+    open_connections: HashMap<SocketAddr,TcpStream>,
+}
+
+impl Listener{
+    pub async fn new(host:&str,port:&str,)-> io::Result<Listener>{
+        if host == "" || port == ""{
+            let listener = TcpListener::bind("127.0.0.1:8080").await?;
+            info!("Listener is running");
+            return Ok(Listener{listener: listener, open_connections: HashMap::new()})
+        }
+        let addr = format!("{}:{}",host,port);
+        let listener = TcpListener::bind(addr).await?;
+        info!("Listener is running");
+        return Ok(Listener{listener:listener, open_connections: HashMap::new()})
+    }
+    /*pub async fn start_listener(&mut self,host: &str, port: &str){
+
+    }*/
+    pub async fn listen(&mut self)-> io::Result<()>{
+        loop{
+            let (stream,addr) = self.listener.accept().await?;
+            if !self.open_connections.contains_key(&addr){
+                self.open_connections.insert(addr,stream);
+                info!("User {} has connected. Total: {}",addr,self.open_connections.len());
+            }
         }
     }
 }
