a5c048c93701da90c094ec8a3ecbf65f3eb61357
MKSafe <49353384+MKSafe@users.noreply.github.com>
2025-12-05 12:53:14 +0100

    fix: add all final cli args

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -5,7 +5,25 @@ use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
+#[command(group(
+  clap::ArgGroup::new("operation")
+    .required(true)
+    .args(["upload", "download", "list"]),
+))]
 pub struct Args {
-    // Path to file
-    pub file_path: PathBuf,
+    ///  IP of the server (host is added automatically)
+    #[arg(short, long)]
+    pub ip: String,
+
+    /// Upload file to server
+    #[arg(short, long, group = "operation")]
+    pub upload: Option<PathBuf>,
+
+    /// Download file from server
+    #[arg(short, long, group = "operation")]
+    pub download: Option<String>,
+    
+    /// List all files on server
+    #[arg(short, long, group = "operation")]
+    pub list: bool,
 }
