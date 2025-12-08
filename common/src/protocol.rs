ef2fb4749cc78ca37e503be185a1f2429b269fb7
NewLifeStyle101 <damian.myszko@gmail.com>
2025-12-08 13:44:20 +0000

    feat: sets up the skeleton of the read function

--- common/src/protocol.rs
+++ common/src/protocol.rs
@@ -12,10 +12,10 @@ impl ProtocolConnection{
         //returns a new connection object
         Ok(ProtocolConnection{_stream})
     }
-
-    pub async fn send<T>(&self,data : T)-> io::Result<bool>{
-        //turns data into bytes and gets the length
-        let data_as_bytes = data.to_bytes();
+    
+    pub async fn send_header(&self, header: FileHeader)->io::Result<bool>{
+        //turns data into bytes and gets the length0.
+        let data_as_bytes = header.to_bytes();
         let data_byte_len = data_as_bytes.len() as u32;
         //initialises a buffer with the size of the data length prefix and data length
         let mut buffer = Vec::with_capacity(4+data_as_bytes.len());
@@ -25,11 +25,14 @@ impl ProtocolConnection{
         buffer.extend_from_slice(&data_as_bytes);
 
         loop{
+            //checks the streams state for writability 
             match self.stream.ready(Interest::WRITEABLE).await{
                 Ok(state)=>{
                     if state.is_writable(){
+                        //if the state is writeable send the packet
                         match self.stream.try_write.await{
                             Ok(n)=>{
+                                //check if the whole packet has been sent
                                 if n == packet.len(){
                                     return Ok(true);
                                 }else{
@@ -46,18 +49,48 @@ impl ProtocolConnection{
 
                     }else if state.is_write_closed(){
                         info!("The socket you are trying to write to is not accessible");
-                        break;
+                        return Ok(false);
                     }
                 }
                 Err(e)=>{
+                    //if there is an error with retriving the socket state print the error to console 
                     error!("The following error has occured: {}",e)
+                    return Ok(false);
                 }
             };
         }
     }
 
-    pub async fn read(&self)-> io::Result<Vec<u8>>{
-        //TODO
+   /* pub async fn send_file(&self,file : Vec<u8>)-> io::Result<bool>{
+        loop{
+            //todo
+        
+        }
+    }*/
+
+    pub async fn read(&self,buffer_len : Option<u8>)-> io::Result<Vec<u8>>{
+        let mut len_buff:Option<Vec<u8>> = None;
+        let mut body_buff:Option<Vec<u8>> = None;
+        if buffer_len.is_none() {
+            len_buff = Some([Vec<u8>::with_capacity(4)]);
+        }
+        if len_buff.is_some(){
+
+        }
+        match self.stream.ready(Interest::READABLE).await{
+            Ok(state)=>{
+                if state.is_readable(){
+                    self.stream.try_read()
+                }else if state.is_read_closed(){
+                
+                }
+
+            }
+            Err(e)=>{
+                error!("An error has occured trying to read data: {}",e);
+                return Ok([0]);
+            }
+        }
         Ok(Vec<u8>::new())
     }
 }
