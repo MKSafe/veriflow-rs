2edace04634fa5904ac4e5c1b724032088a799bf
MKSafe <49353384+MKSafe@users.noreply.github.com>
2025-12-29 15:16:42 +0100

    feat(client): add file upload offline logic and main refactor

--- client/src/transfer.rs
+++ client/src/transfer.rs
@@ -7,3 +7,25 @@ use tokio::io::AsyncWriteExt;
 use common::{FileHeader, Command, VeriflowError};
 use crate::{hashing, ui};
 
+pub async fn upload_file(path: &Path, ip: &str) -> common::Result<()> {
+  // Offline Logic (Validation)
+  
+  // get file with tokio (VeriflowError if it doesn't exist)
+  let mut file = File::open(path).await?;
+
+  // get file metadata
+  let file_metadata = file.metadata().await?;
+  let file_size = file_metadata.len();
+
+  // Hashing
+  println!("Starting Hashing...");
+  let file_hash = hashing::hash_file(path).await?;
+
+  println!("File Hash: {file_hash}");
+
+
+  // Connect to server
+
+
+  Ok(())
+}
