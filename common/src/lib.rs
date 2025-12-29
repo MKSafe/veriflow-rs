24e7c663708bfd6658988d77811b1681e9854d8e
NewLifeStyle101 <damian.myszko@gmail.com>
2025-12-29 01:14:25 +0000

    feat: implements a async test func to send a header

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use tokio::net::{TcpListener, TcpStream};
 pub mod protocol;
+use protocol::ProtocolConnection;
 // cli command arg
 // PartialEQ for unit test
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -49,6 +50,31 @@ mod tests {
 
         assert_eq!(original_file_header, deserialised_json);
     }
+    #[tokio::test]
+    async fn test_protocol_read_and_write() -> Result<(), Box<dyn std::error::Error>> {
+        let stream = TcpStream::connect("127.0.0.1:8080").await?;
+        let connection = ProtocolConnection::new(stream).await?;
 
-    fn test_protocol_read_and_write() {}
+        // set file name
+        let file_name: &str = "img.png";
+
+        // instantiate file header
+        let original_file_header: FileHeader = FileHeader {
+            command: Command::Download,
+            name: String::from(file_name),
+            size: 4001,
+            hash: String::from("abc123def"),
+        };
+        // serialise to JSON (Struct -> String)
+        let json_string_wrapped = serde_json::to_string(&original_file_header);
+        let json_string = json_string_wrapped.unwrap();
+        let result = connection.send_header(&json_string).await;
+        let header_length = connection.read_prefix().await?;
+        let byte_header = connection.read_body(header_length).await?;
+        let header = String::from_utf8_lossy(&byte_header);
+        assert_eq!(json_string,header);
+
+        Ok(())
+
+    }
 }
