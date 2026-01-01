1332c7f4d342a4bf70a954b7a0d3b1228edb2471
NewLifeStyle101 <damian.myszko@gmail.com>
2026-01-01 23:20:31 +0000

    perf: Removed common dependency on the server and added common dependency to server

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
 pub mod protocol;
-pub use protocol::ProtocolConnection;
 use thiserror::Error;
 
 // cli command arg
@@ -43,8 +42,6 @@ pub type Result<T> = std::result::Result<T, VeriflowError>;
 #[cfg(test)]
 mod tests {
     use super::*;
-    pub use server::server::Listener;
-    use tokio::net::TcpStream;
     // Test Serialisation and Deserialisation
     #[test]
     fn test_file_header_serialisation() {
@@ -71,51 +68,6 @@ mod tests {
         let deserialised_json = deserialised_json_wrapped.unwrap();
 
         assert_eq!(original_file_header, deserialised_json);
-    }
-    #[tokio::test]
-    async fn test_protocol_read_and_write(
-    ) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        //made to avoid veriflow error
-        type AnyResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-        //creates a server
-        let mut server = Listener::new("127.0.0.1", "0").await?;
-        let addr = server.local_addr()?;
-        let server_task: tokio::task::JoinHandle<AnyResult<()>> = tokio::spawn(async move {
-            let stream = server.accept_once().await?;
-            let conn = ProtocolConnection::new(stream).await?;
-
-            let len = conn.read_prefix().await?;
-            let body = conn.read_body(len).await?;
-            conn.send_header(&String::from_utf8_lossy(&body)).await?;
-
-            Ok(())
-        });
-        let stream = TcpStream::connect(addr).await?;
-        let connection = ProtocolConnection::new(stream).await?;
-
-        // set file name
-        let file_name: &str = "img.png";
-
-        // instantiate file header
-        let original_file_header: FileHeader = FileHeader {
-            command: Command::Download,
-            name: String::from(file_name),
-            size: 4001,
-            hash: String::from("abc123def"),
-        };
-        // serialise to JSON (Struct -> String)
-        let json_string_wrapped = serde_json::to_string(&original_file_header);
-        let json_string = json_string_wrapped.unwrap();
-        let _result = connection.send_header(&json_string).await?;
-        let header_length = connection.read_prefix().await?;
-        let byte_header = connection.read_body(header_length).await?;
-        let header = String::from_utf8_lossy(&byte_header);
-        assert_eq!(json_string, header);
-        match server_task.await {
-            Ok(res) => res?,
-            Err(e) => panic!("server task panicked: {e}"),
-        }
-        Ok(())
     }
     // Test VeriFlow error type struct
     #[test]
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,1 +1,54 @@
 pub mod server;
+
+#[cfg(test)]
+mod test{
+    pub use common::protocol::ProtocolConnection;
+    pub use common::{Command, FileHeader};
+    use tokio::net::TcpStream;
+    use crate::server::Listener;
+    #[tokio::test]
+    async fn test_protocol_read_and_write(
+    ) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        //made to avoid veriflow error
+        type AnyResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+        //creates a server
+        let mut server = Listener::new("127.0.0.1", "0").await?;
+        let addr = server.local_addr()?;
+        let server_task: tokio::task::JoinHandle<AnyResult<()>> = tokio::spawn(async move {
+            let stream = server.accept_once().await?;
+            let mut conn = ProtocolConnection::new(stream).await?;
+
+            let len = conn.read_prefix().await?;
+            let body = conn.read_body(len).await?;
+            conn.send_header(&String::from_utf8_lossy(&body)).await?;
+
+            Ok(())
+        });
+        let stream = TcpStream::connect(addr).await?;
+        let mut connection = ProtocolConnection::new(stream).await?;
+
+        // set file name
+        let file_name: &str = "img.png";
+
+        // instantiate file header
+        let original_file_header: FileHeader = FileHeader {
+            command: Command::Download,
+            name: String::from(file_name),
+            size: 4001,
+            hash: String::from("abc123def"),
+        };
+        // serialise to JSON (Struct -> String)
+        let json_string_wrapped = serde_json::to_string(&original_file_header);
+        let json_string = json_string_wrapped.unwrap();
+        let _result = connection.send_header(&json_string).await?;
+        let header_length = connection.read_prefix().await?;
+        let byte_header = connection.read_body(header_length).await?;
+        let header = String::from_utf8_lossy(&byte_header);
+        assert_eq!(json_string, header);
+        match server_task.await {
+            Ok(res) => res?,
+            Err(e) => panic!("server task panicked: {e}"),
+        }
+        Ok(())
+    }
+}
